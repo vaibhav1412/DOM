ca21582d74630457d2262b58c0f8214b14b98c25
Vaibhav Rote <vaibhav.edu.online.07@gmail.com>
2025-12-15 13:50:15 +0530

    DOM class Operations

---
+++ DOM OPRATIONS/04_class operations/script.js
@@ -0,0 +1,20 @@
+  let el = document.getElementById("box");
+
+  // add class
+  el.classList.add("highlight");
+
+  // remove class
+  // el.classList.remove("highlight");
+
+  // toggle class
+  // el.classList.toggle("highlight");
+
+  
+/*
+theory: 
+
+1.add() → adds a class
+2.remove() → removes a class
+3.toggle() → adds if not present, removes if present
+
+*/
