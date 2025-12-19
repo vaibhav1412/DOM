09ff1b2a1ef0829dc0e3a548f259e8b06b5439bd
Vaibhav Rote <vaibhav.edu.online.07@gmail.com>
2025-12-19 11:10:06 +0530

    some changes file name

---
+++ 02_DOM EVENTS/02_2_EVENT OBJECT/script.js
@@ -0,0 +1,13 @@
+/*
+Q. What is the event object?
+
+ ->When an event happens (click, input, key press),
+JavaScript automatically gives extra information about that event.
+
+That information is stored in a variable usually named event (or e).
+
+*/
+
+let btn = document.getElementById("btn");
+let msg = document.getElementById("msg");
+let box = document.getElementById("box");
